ee2e528569864f19a1a6d1b25a479a39148bbd84
sathishkumar-g <50577411+sathishkumar-g@users.noreply.github.com>
2021-05-25 01:34:33 +0530

    removing eureka

--- src/main/java/com/sugysri/birthday/getgiftrecordservice/GetGiftRecordServiceApplication.java
+++ src/main/java/com/sugysri/birthday/getgiftrecordservice/GetGiftRecordServiceApplication.java
@@ -2,10 +2,9 @@ package com.sugysri.birthday.getgiftrecordservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class GetGiftRecordServiceApplication {
 
 	public static void main(String[] args) {
